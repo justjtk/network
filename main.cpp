2f4af24bb0c584aca27a0cd1c3fcabb47cbfaede
“j_t_k” <270337709@qq.com>
2022-05-08 22:07:58 +0800

    添加RSA加密

--- main.cpp
+++ main.cpp
@@ -2,14 +2,25 @@
 
 #include <QApplication>
 #include"gmp.h"
+#include"RSA.h"
+#include<QDebug>
+#include<iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    mpz_t t;
-    mpz_init(t);
-    mpz_ui_pow_ui(t,2,100);
-    gmp_printf("%Zd\n",t);
+    key_pair * p = get_key_pair();
+    qDebug()<<"n = "<<p->n<<endl;
+    qDebug()<<"d = "<<p->d<<endl;
+    qDebug()<<"e = "<<p->e<<endl;
+    string buf="你好";
+    string cipher_text = encrypt_str(buf, p->n, p->e);
+    qDebug()<<"密文为"<<cipher_text.c_str()<<endl;
+    string plain_text = decrypt_str(cipher_text, p->n, p->d);
+    qDebug()<<""<<plain_text.c_str()<<endl;
+
     return a.exec();
+
 }
